a71146d2696e7b21bd262b66a9ffd66277aedea9
shyam kachhadiya <shyam1ss15@gmail.com>
2023-09-02 16:23:50 +0000

    base api reverted

--- lib/api/base_api.dart
+++ lib/api/base_api.dart
@@ -1,5 +1,5 @@
-const BASE_API = "http://localhost:8080";
-// const BASE_API = "https://sheraa-backend-production.up.railway.app";
+// const BASE_API = "http://localhost:8080";
+const BASE_API = "https://sheraa-backend-production.up.railway.app";
 
 // enum API_Decision { NONE, Only_Success, Only_Failure, BOTH }
 
